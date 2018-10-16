91e37badbe9a337ef2c407b2fe5d6e2fd1229ad0
Tim Lavelle <tim@digitised.io>
2018-10-16 13:15:18 +1100

    Added ability to countdown hours, minutes and seconds and control the labels and interval of the countdown refresh

--- MMM-CountDown.js
+++ MMM-CountDown.js
@@ -2,8 +2,15 @@ Module.register("MMM-CountDown",{
 	// Default module config.
 	defaults: {
 		event: "New Millenium:",
-		date: "3000-01-01"
-		
+		date: "3000-01-01",
+		showHours: true,
+		showMinutes: true,
+		showSeconds: true,
+		customInterval: 1000,
+		daysLabel: 'd',
+		hoursLabel: 'h',
+		minutesLabel: 'm',
+		secondsLabel: 's',		
 	},
 
 	// Set translations
@@ -19,35 +26,45 @@ Module.register("MMM-CountDown",{
 		var self = this;
 		setInterval(function() {
 			self.updateDom(); // no speed defined, so it updates instantly.
-		}, 1000*60*10); //perform every 10 minutes
+		}, this.config.customInterval); 
 	},
-	
+
 	// Update function
 	getDom: function() {
 		var wrapper = document.createElement("div");
-		
+
 		var timeWrapper = document.createElement("div");
 		var textWrapper = document.createElement("div");
-		var unitWrapper = document.createElement("sup");
-		
+
 		textWrapper.className = "align-left week dimmed medium";
 		timeWrapper.className = "time bright xlarge light";
-		unitWrapper.className = "dimmed";
-		
 		textWrapper.innerHTML=this.config.event;
-		
+
 		var today = new Date(Date.now());
 		var target = new Date(this.config.date);
-		var timeDiff = target.getTime() - today.getTime();
-		var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+		var timeDiff = target - today;
+
+		// Set days, hours, minutes and seconds
+		var diffDays = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+		var diffHours = Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var diffMinutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
+		var diffSeconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
 		
-		timeWrapper.innerHTML = diffDays;
-		unitWrapper.innerHTML = " " + this.translate("DAYS");
-				
+		// Build the output
+		var hrs = '';
+		var mins = '';
+		var secs = '';
+		var days = diffDays + this.config.daysLabel;
+
+		if(this.config.showHours == true) hrs = diffHours + this.config.hoursLabel;
+		if(this.config.showMinutes == true) mins = diffMinutes + this.config.minutesLabel;
+		if(this.config.showSeconds == true) secs = diffSeconds + this.config.secondsLabel;
+
+		timeWrapper.innerHTML = days + hrs + mins + secs;
+
 		wrapper.appendChild(textWrapper);
-		timeWrapper.appendChild(unitWrapper);
 		wrapper.appendChild(timeWrapper);
-		
+
 		return wrapper;
 	}
 });
