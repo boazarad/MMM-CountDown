49f5e3a1bda94e0e38890da7652d1a1611bd15b3
maartenpaauw <maartenpaauw@gmail.com>
2019-09-07 15:40:35 +0200

    style: converted some spaces to tabs because tabs are used

--- MMM-CountDown.js
+++ MMM-CountDown.js
@@ -39,7 +39,7 @@ Module.register("MMM-CountDown",{
 		// Set days, hours, minutes and seconds
 		var diffDays = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
 		var diffHours = Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var diffMinutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
+		var diffMinutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
 		var diffSeconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
 		
 		// Build the output
